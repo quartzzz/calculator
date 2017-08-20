17d44b3f1f9a3c7afddd9a86c4c58f43e869c593
Quartzzz <Jalen@immamac.com>
2017-08-19 21:12:27 -0500

    Trying to make a button

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 "use strict"
 
-let numberOneExtracted = document.getElementById("numberInputOne")
+// Add Element to body
+function addElementToBody(elementType, innerText) {
+    // Create Node
+    let elementCreated = document.createElement(`${elementType}`);
+
+    // New type depending
+    String(innerText);
+    switch (elementCreated.getElementsByTagName) {
+        case "button":
+            elementCreated.innerHTML = innerText;
+            break;
+        case "label":
+            elementCreated.innerHTML = innerText;
+            break;
+    };
+
+    // Add to page
+    let newDiv = document.createElement("div");
+    newDiv.appendChild(elementCreated);
+
+    let placeAboveDiv = document.getElementById("currentDiv");
+    document.body.insertBefore(newDiv, placeAboveDiv);
+
+};
+
+addElementToBody("button", "ha_lol");
+
+function buttonLabel() {
+    let buttonProperty = document.getElementsByTagName("button");
+
+    if (buttonProperty == document.getElementsByTagName("button")) {
+        get.addEventListener("click", function() {
+            alert("wew ima gay");
+        });
+    };
+};
